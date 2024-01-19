58056d99abe6fc02b396cfb4d39743d9c04e0dc7
wgzhao <wgzhao@gmail.com>
2024-01-19 13:43:28 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## Apereo CAS based SSO
+
+基于 [Apereo CAS](https://apereo.github.io/cas) 的 SSO 实现，基于最新的 CAS 7.0 版本，底层 SpringBoot 版本为 3.2.1
+
+采取 Overlay 模式开发。
+
+在官方版本的基础上，主要做了以下改动
+
+- 新增了名为 `gp51` 的主题
+- 修改了 `casLoginView.html` 页面，内嵌了企业微信扫码登录逻辑
+- 增加了自定义认证，和项目 [sso-wecom-api](https://gitlab.gp51.com/sso-wecom-api) 配合，实对企业微信扫描的认证。
---
+++ src/main/java/com/gp51/sso/config/WeComOverlayConfiguration.java
@@ -0,0 +1,33 @@
+package com.gp51.sso.config;
+
+import com.gp51.sso.cas.WeComAuthenticationHandler;
+import org.apereo.cas.authentication.AuthenticationEventExecutionPlanConfigurer;
+import org.apereo.cas.authentication.AuthenticationHandler;
+import org.apereo.cas.authentication.principal.DefaultPrincipalFactory;
+import org.apereo.cas.configuration.CasConfigurationProperties;
+import org.apereo.cas.services.ServicesManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+@AutoConfiguration
+@EnableConfigurationProperties(CasConfigurationProperties.class)
+public class WeComOverlayConfiguration {
+
+    @Autowired
+    @Qualifier("servicesManager")
+    private ServicesManager servicesManager;
+
+    @Bean
+    public AuthenticationHandler weComAuthenticationHandler() {
+        return new WeComAuthenticationHandler(WeComAuthenticationHandler.class.getSimpleName(), servicesManager, new DefaultPrincipalFactory(), 1);
+    }
+
+    @Bean
+    public AuthenticationEventExecutionPlanConfigurer weComAuthenticationEventExecutionPlanConfigurer(@Qualifier("weComAuthenticationHandler") final AuthenticationHandler weComAuthenticationHandler) {
+        return plan -> plan.registerAuthenticationHandler(weComAuthenticationHandler);
+    }
+}
+
