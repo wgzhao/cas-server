497cb36c37cdc7ef3deb9bcbedafa4850abb94cd
wgzhao <wgzhao@gmail.com>
2024-03-25 19:44:24 +0800

    Add support for multiple wecom authorities

--- src/main/java/com/gp51/sso/cas/WeComAuthenticationHandler.java
+++ src/main/java/com/gp51/sso/cas/WeComAuthenticationHandler.java
@@ -1,45 +1,52 @@
 package com.gp51.sso.cas;
 
 import com.gp51.sso.PasswordUtil;
+import jakarta.servlet.http.HttpServletRequest;
 import org.apereo.cas.authentication.AcceptUsersAuthenticationHandler;
 import org.apereo.cas.authentication.AuthenticationHandlerExecutionResult;
 import org.apereo.cas.authentication.MessageDescriptor;
 import org.apereo.cas.authentication.credential.UsernamePasswordCredential;
+import org.apereo.cas.authentication.principal.Principal;
 import org.apereo.cas.authentication.principal.PrincipalFactory;
 import org.apereo.cas.services.ServicesManager;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.security.auth.login.FailedLoginException;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 public class WeComAuthenticationHandler extends AcceptUsersAuthenticationHandler
 {
     private static final List<MessageDescriptor> warningList = new ArrayList<>();
 
-    private ServicesManager servicesManager;
     public WeComAuthenticationHandler(String name, ServicesManager servicesManager, PrincipalFactory principalFactory, Integer order) {
-        super(name);
+        super(name, servicesManager, principalFactory, order, null);
     }
 
     @Override
     protected AuthenticationHandlerExecutionResult authenticateUsernamePasswordInternal(
             UsernamePasswordCredential credential,
             String originalPassword) throws Throwable {
+
+        HttpServletRequest requestObj = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        for (Map.Entry<String, String[]> obj : requestObj.getParameterMap().entrySet()) {
+            System.out.println(obj);
+        }
         // todo 定义自己的验证方法
-        String username = credential.getUsername();
-        if (PasswordUtil.isWecomPassword(originalPassword)) {
+        //$1$2cf88021c6d36b9a7d86373a3937cccc2444
+        if (PasswordUtil.isWecomPassword(originalPassword) & PasswordUtil.isPseudoPassword(originalPassword)) {
             // decrypt password
-            //$1$2cf88021c6d36b9a7d86373a3937cccc2444
-            if (PasswordUtil.isPseudoPassword(originalPassword)) {
-                return createHandlerResult(credential,
-                        principalFactory.createPrincipal(username), warningList);
-            } else {
-                throw new FailedLoginException("Sorry, you are a failure!");
-            }
+            Map<String, List<Object>> attributes = new HashMap<>();
+            attributes.put("authType", List.of("wecom"));
+            Principal principal = principalFactory.createPrincipal(credential.getUsername(), attributes);
+            return createHandlerResult(credential, principal, warningList);
         } else {
             // invoke super auth method
             return super.authenticateUsernamePasswordInternal(credential, originalPassword);
--- src/main/resources/static/themes/gp51/js/wx.js
+++ src/main/resources/static/themes/gp51/js/wx.js
@@ -1,51 +1,64 @@
-function wxQR() {
-    weParam = {
-        "id": "wx_gp51",
-        "appid": "wwfbeaf1a16d7f77b4",
-        "agentid": "1000087",
-        "redirect_uri": "https%3A%2F%2Fgzg.gp51.com%3A8001%2Fcas%2Flogin",
-        "state": "11019",
-        "href": "",
+function json2select(json) {
+    var html = '<select id="corp" onchange="getWecomQR()">';
+    for (var key in json) {
+        html += '<option value="' + json[key].entityId + '">' + json[key].name + '</option>';
     }
+    html += '</select>';
+    return html;
+}
 
-    var params = new URLSearchParams(window.location.search)
-    var code = params.get('code')
-    if (code != null) {
-        console.log("current code = " + code);
-        // ajax post to the wechat token url
-        var tokenUrl = "/cas/v1/wecom/getAccessToken";
+function getWecomQR() {
+    var corpid = $('#corp').val();
+    console.log(corpid);
+    var corpInfo = JSON.parse(localStorage.corpInfo);
+    var weParam = corpInfo[corpid];
+    weParam["id"] = "wx_gp51";
+    // add redirect_uri via current location
+    var redirect_uri;
+    if (location.port == "") {
+        redirect_uri = location.protocol + "//" + location.hostname + "/cas/v1/wecom/callback";
+    } else {
+        redirect_uri = location.protocol + "//" + location.hostname + ":" + location.port + "/cas/v1/wecom/callback";
+    }
+    weParam["redirect_uri"] = redirect_uri;
+    console.log(weParam);
+    window.WwLogin(weParam);
+}
 
-        // specify content-type
+function getCorpInfo() {
+    if (localStorage.corpInfo == null) {
         $.ajax({
-            type: "GET",
-            url: tokenUrl,
-            contentType: "application/json"}).done(function (data, status) {
-            if (status === "success") {
-                var token = data["access_token"];
-                console.log(token);
-                $.ajax({
-                    type: "GET",
-                    url: "/cas/v1/wecom/getUserInfo", 
-                    contentType: "application/json",
-                    data: { "code": code, "access_token": token }
-                }).done(function (data, status) {
-                    if (status === "success") {
-                        console.log(data);
-                        // var UserId = data['UserId'];
-                        // fill form with userId as username and fixed password
-                        $("#username").val(data['UserId']);
-                        $("#password").val(data['password']);
-                        // then, submit the form
-                        $("#fm1").submit();
-                    }
-                });
+            url: '/cas/v1/wecom/corpInfo',
+            type: 'GET',
+            dataType: 'json',
+            contentType: 'application/json',
+            success: function (data) {
+                $('#corpDiv').html(json2select(data));
+                localStorage.corpInfo = JSON.stringify(data);
             }
         });
+    } else {
+        console.log("get from localStorage");
+        var data = JSON.parse(localStorage.corpInfo);
+        $('#corpDiv').html(json2select(data));
+    }
+}
+
+function wxQR() {
+
+    var params = new URLSearchParams(window.location.search)
+    var username = params.get('username')
+    if (username != null) {
+        $("#username").val(username);
+        $("#password").val(params.get('password'));
+        // then, submit the form
+        $("#fm1").submit();
     } else {
         // initial wecom qrcode
         if (params.get('service')) {
             weParam['redirect_uri'] = weParam['redirect_uri'] + '?service=' + params.get('service');
         }
-        window.WwLogin(weParam);
+        getCorpInfo();
+        getWecomQR()
     }
 }
